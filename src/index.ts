56dff591e3eae7b3de4505f2cc7df67e2b0220a3
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-10-28 12:26:05 -0300

    feat: add query sample

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,46 @@
-import getPoolInfo from '@balancer/getPoolInfo';
-import getVaultContract from '@balancer/getVaultContract';
+import vaultAbi from './abis/Vault.json';
 import * as dotenv from 'dotenv';
 import { ethers } from 'ethers';
 
 dotenv.config();
 
+const vaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
+
+const getProvider = () => {
+  const infuraEndpoint = process.env.INFURA_ENDPOINT;
+
+  if (!infuraEndpoint) throw Error('Infura endpoint is not defined.');
+
+  return new ethers.providers.JsonRpcProvider(infuraEndpoint);
+};
+
+const getVaultContract = () => {
+  const provider = getProvider();
+
+  return new ethers.Contract(vaultAddress, vaultAbi, provider);
+};
+
 (async function () {
-  const poolAddresses = Object.keys(pools).map(function (key) {
-    return poolAddresses[key];
-  });
-
-  const poolsInfo = await Promise.all(
-    poolAddresses.map(async (poolId) => getPoolInfo(poolId)),
-  );
-
-  // console.log(JSON.stringify(poolsInfo, null, 4));
-
-  let pricePairs = { BAL: null, DAI: null, WETH: null };
-
-  poolsInfo.map(({ tokens }) => {
-    const [first, second] = tokens;
-
-    const relativePrice =
-      (first.balance / second.balance) * (second.weight / first.weight);
-
-    const nodes = {
-      [first.symbol]: {
-        [second.symbol]: relativePrice,
-      },
-      [second.symbol]: {
-        [first.symbol]: 1 / relativePrice,
-      },
-    };
-
-    for (const token in nodes) {
-      if (!pricePairs[token]) {
-        Object.assign(pricePairs, { ...pricePairs, [token]: nodes[token] });
-      } else {
-        Object.assign(pricePairs, {
-          ...pricePairs,
-          [token]: { ...pricePairs[token], ...nodes[token] },
-        });
-      }
-    }
-    //console.log(nodes);
-  });
-  //console.log(pricePairs);
-
-  const paths = [];
-
-  for (const token in pricePairs) {
-    for (const bar in pricePairs[token]) {
-      const first = { pair: [token, bar], price: pricePairs[token][bar] };
-
-      const foo = Object.keys(pricePairs[bar]).filter(
-        (item) => item !== token,
-      )[0];
-
-      const second = { pair: [bar, foo], price: pricePairs[bar][foo] };
-
-      const third = { pair: [foo, token], price: pricePairs[foo][token] };
-
-      paths.push([first, second, third]);
-    }
-  }
-
-  const rates = paths.map((path) => {
-    return path
-      .map(({ price }) => price)
-      .reduce((acc, cur) => {
-        return acc * cur;
-      });
-  });
 
   const kind = 0;
 
   const assets = [
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    '0xba100000625a3754423978a60c9317c58a424e3D',
   ];
 
   const funds = {
-    sender: '0xE64F06F695De4C4E21BA8F383a2675Fd884Ac720',
-    recipient: '0xE64F06F695De4C4E21BA8F383a2675Fd884Ac720',
+    sender: '0x0000000000000000000000000000000000000000',
+    recipient: '0x0000000000000000000000000000000000000000',
   };
 
   const swaps = [
     {
       poolId:
-        '0x0297e37f1873d2dab4487aa67cd56b58e2f27875000200000000000000000003',
+        '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014',
       assetInIndex: '1',
       assetOutIndex: '0',
-      amount: ethers.utils.parseEther('0.05'),
-      userData: '0x',
-    },
-    {
-      poolId:
-        '0xa6f548df93de924d73be7d25dc02554c6bd66db500020000000000000000000e',
-      assetInIndex: '0',
-      assetOutIndex: '1',
-      amount: ethers.utils.parseEther('0.05'),
+      amount: ethers.utils.parseEther('1'),
       userData: '0x',
     },
   ];
@@ -110,33 +51,5 @@ dotenv.config();
 
   const result = await vaultContract.callStatic.queryBatchSwap(...args);
 
-  console.log(
-    'res',
-    result.map((res) => ethers.utils.formatEther(res)),
-  );
-
-  console.log(assets, paths[1]);
-
-  // 1. get pool data () from the pools you want to work with;
-  // 2. get token proportion from pool;
-  // 3. get price reference from elsewhere;
+  console.log(result);
 })();
-
-// BAL DAI WETH
-// WETH DAI BAL
-
-// BAL WETH DAI
-// DAI WETH BAL
-
-// WETH BAL DAI
-// DAI BAL WETH
-
-// (WETH -> BAL) -> (BAL -> DAI) -> (DAI -> WETH)
-// ((114.5)*19.33)*1/2177.35 = 1,016504007164673
-
-// (DAI -> BAL) -> (BAL -> WETH) -> (WETH -> DAI)
-// ((1/19.33)*(1/114.50))*(2177.35) = 0,983763952676677
-
-// (WETH -> BAL) == (BAL -> ETH)
-// (DAI -> WETH) == (WETH -> DAI)
-// (BAL -> WETH) == (WETH -> BAL)
